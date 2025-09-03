9b45a4fa8729bcec64fe4b748145db32eebd1e6d
ADHARSH KUMAR SINGH <146474181+adrsshh@users.noreply.github.com>
2025-09-03 18:38:36 +0530

    Update About.jsx

--- components/About.jsx
+++ components/About.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const About = () => {
   return (
-    <div className='text-amber-50 font-mono text-9xl'>Coming Soon</div>
+    <div className='text-amber-50 font-mono text-4xl'>Coming Soon</div>
   )
 }
 
